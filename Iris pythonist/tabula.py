f670a04cafff92ed87e0dc34adbd96a3b2ff9e1d
Cristian Armijo <correode@cristianarmijo.es>
2025-12-06 16:16:41 -0500

    carga de archivo

---
+++ Iris pythonist/tabula.py
@@ -0,0 +1,11 @@
+import tabula
+#test 1test
+
+
+# Read pdf into a list of DataFrame
+dfs = tabula.read_pdf("C:/Users/Iris/Desktop/test.pdf ", pages='all')
+
+tabula.convert_into("C:/Users/Iris/Desktop/test.pdf", "output.csv", output_format="csv", pages='all')
+
+# convert all PDFs in a directory
+tabula.convert_into_by_batch("C:/Users/Iris/Desktop/", output_format='csv', pages='all')
